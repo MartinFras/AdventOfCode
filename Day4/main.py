56b8d30be42d26dd961e555fc2caa0d998eafd54
MartinFras <martin.fras@gmail.com>
2019-12-17 11:23:55 +0100

    Added solution day 4 part 2 to main.py

--- Day4/main.py
+++ Day4/main.py
@@ -4,12 +4,22 @@ def main():
     #start = 100000
     #finish = 200000
     counter = 0
+    possible_passwords = list()
     for i in range(start, finish+1):
         password = comparison(i)
         if(checkpassword(password)):
             counter += 1
+            possible_passwords.append(password)
             #print(password)
     print("Solution part 1: ",counter)
+
+    counter = 0
+    for i in range(0,len(possible_passwords)):
+        password = possible_passwords[i]
+        if (checkpassword_parttwo(password)):
+            counter += 1
+            # print(password)
+    print("Solution part 2: ", counter)
 
 def comparison(password):
     password_string = str(password)
@@ -36,20 +46,12 @@ def checkpassword(password):
 
 ######### PART 2 #########
 def checkpassword_parttwo(password):
-    if (password[0] <= password[1]):
-        if (password[1] <= password[2]):
-            if (password[2] <= password[3]):
-                if (password[3] <= password[4]):
-                    if (password[4] <= password[5]):
-                        if (password[0] == password[1] or password[1] == password[2] or password[2] == password[3] or
-                                password[3] == password[4] or password[4] == password[5]):
-                            return 1
-                        return 0
-                    return 0
-                return 0
-            return 0
-        return 0
+    for i in range(0, len(password)):
+        counter = 0
+        for j in range(0, len(password)):
+            if(password[i] == password[j]):
+                counter += 1
+        if(counter == 2):
+            return 1
     return 0
-
-
 main()
